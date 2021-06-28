4879a632cbc010058546656bb9f00147bf64b978
Joshuria <joshuria@gmail.com>
2021-06-29 01:15:37 +0800

    Fix: pytest might fail on some system.

--- test/conftest.py
+++ test/conftest.py
@@ -7,13 +7,15 @@ sys.path.insert(
     0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 )
 
+
 from app import create_app
 
 @pytest.fixture(scope='session', autouse=True)
 def client():
+    """Testing client fixture."""
     app = create_app(True)
-    client = app.test_client()
+    testClient = app.test_client()
     cxt = app.app_context()
     cxt.push()
-    yield client
+    yield testClient
     cxt.pop()
